00d35f330e2d3c7528bd946e68e7357f2b7718fa
David Kua <david.kua@pressly.com>
2014-06-27 19:09:34 +0000

    Implemented Wrap function to create http.Handlers.
    The Wrap() function now takes certain functions with the right signature
    and turns them into functions that can be used as middleware.

--- auth/basic_auth.go
+++ auth/basic_auth.go
@@ -7,32 +7,45 @@ import (
 	"strings"
 )
 
-func Unauthorized(realm string, w http.ResponseWriter) {
-	w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Basic realm="%s"`, realm))
-	w.WriteHeader(http.StatusUnauthorized)
-	w.Write([]byte("Please authenticate with the proper API user and password!\n"))
-}
+type AuthFunc func(*http.Request, []string) bool
 
-func BasicAuth(user, pass, realm string) func(http.Handler) http.Handler {
-	f := func(h http.Handler) http.Handler {
-		authString := fmt.Sprintf("%s:%s", user, pass)
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			auth := r.Header.Get("Authorization")
-			if !strings.HasPrefix(auth, "Basic ") {
-				Unauthorized(realm, w)
-				return
-			}
-
-			pass, err := decodeAuth(auth[6:])
-			if err != nil || pass != authString {
+func Wrap(f AuthFunc, realm string, secrets ...string) func(http.Handler) http.Handler {
+	h := func(h http.Handler) http.Handler {
+		hn := func(w http.ResponseWriter, r *http.Request) {
+			if !f(r, secrets) {
 				Unauthorized(realm, w)
 				return
 			}
 			h.ServeHTTP(w, r)
 		}
-		return http.HandlerFunc(fn)
+		return http.HandlerFunc(hn)
 	}
-	return f
+	return h
+}
+
+func Unauthorized(realm string, w http.ResponseWriter) {
+	w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Basic realm="%s"`, realm))
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte("Please authenticate with the proper details\n"))
+}
+
+func BasicAuth(user, pass string) func(http.Handler) http.Handler {
+	return Wrap(basicAuth, "Restricted", user, pass)
+}
+
+func basicAuth(r *http.Request, secrets []string) bool {
+	user, pass := secrets[0], secrets[1]
+	authString := fmt.Sprintf("%s:%s", user, pass)
+	auth := r.Header.Get("Authorization")
+	if !strings.HasPrefix(auth, "Basic ") {
+		return false
+	}
+
+	pass, err := decodeAuth(auth[6:])
+	if err != nil || pass != authString {
+		return false
+	}
+	return true
 }
 
 // This function decodes the given string.
