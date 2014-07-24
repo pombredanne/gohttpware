5a4f361250fa98b19c736ab59eb409bdb98fe4a6
David Kua <david.kua@pressly.com>
2014-07-24 20:46:01 +0000

    Added Redis based auth blocking.

--- auth/basic_auth.go
+++ auth/basic_auth.go
@@ -7,28 +7,6 @@ import (
 	"strings"
 )
 
-type AuthFunc func(*http.Request, []string) bool
-
-func Wrap(f AuthFunc, realm string, secrets ...string) func(http.Handler) http.Handler {
-	h := func(h http.Handler) http.Handler {
-		hn := func(w http.ResponseWriter, r *http.Request) {
-			if !f(r, secrets) {
-				Unauthorized(realm, w)
-				return
-			}
-			h.ServeHTTP(w, r)
-		}
-		return http.HandlerFunc(hn)
-	}
-	return h
-}
-
-func Unauthorized(realm string, w http.ResponseWriter) {
-	w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Basic realm="%s"`, realm))
-	w.WriteHeader(http.StatusUnauthorized)
-	w.Write([]byte("Please authenticate with the proper details\n"))
-}
-
 func BasicAuth(user, pass string) func(http.Handler) http.Handler {
 	return Wrap(basicAuth, "Restricted", user, pass)
 }
@@ -41,7 +19,7 @@ func basicAuth(r *http.Request, secrets []string) bool {
 		return false
 	}
 
-	pass, err := decodeAuth(auth[6:])
+	pass, err := decodePlainAuth(auth[6:])
 	if err != nil || pass != authString {
 		return false
 	}
@@ -50,7 +28,7 @@ func basicAuth(r *http.Request, secrets []string) bool {
 
 // This function decodes the given string.
 // Here is where we would put any decryption if required.
-func decodeAuth(auth string) (string, error) {
+func decodePlainAuth(auth string) (string, error) {
 	pass, err := base64.StdEncoding.DecodeString(auth)
 	return string(pass), err
 }
