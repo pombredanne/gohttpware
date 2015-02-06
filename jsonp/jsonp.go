ef2e318dc87cb1a90b02e718f06c1b810b1d897f
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-02-06 16:13:03 -0500

    JSONP middleware to include headers in meta hash

--- jsonp/jsonp.go
+++ jsonp/jsonp.go
@@ -2,6 +2,8 @@ package jsonp
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -22,14 +24,30 @@ func Handle(h http.Handler) http.Handler {
 		h.ServeHTTP(wb, r)
 
 		if strings.Index(wb.Header().Get("Content-Type"), "/json") >= 0 {
-			// Wrap the json data in the js callback function and set the
-			// content type to javascript for the <script>
-			wb.PreBody.Write([]byte(callback + "("))
+			status := wb.Status
+			data := wb.Body.Bytes()
+			wb.Body.Reset()
+
+			resp := &jsonpResponse{
+				Meta: map[string]interface{}{"status": status},
+				Data: data,
+			}
+			for k, v := range wb.Header() {
+				resp.Meta[strings.ToLower(k)] = v[0]
+			}
+			resp.Meta["content-length"] = len(data)
+
+			body, err := json.Marshal(resp)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			wb.Body.Write([]byte(callback + "("))
+			wb.Body.Write(body)
 			wb.Body.Write([]byte(")"))
 
-			contentLength := wb.PreBody.Len() + wb.Body.Len()
 			wb.Header().Set("Content-Type", "application/javascript")
-			wb.Header().Set("Content-Length", strconv.Itoa(contentLength))
+			wb.Header().Set("Content-Length", strconv.Itoa(wb.Body.Len()))
 		}
 
 		wb.Flush()
@@ -37,18 +55,30 @@ func Handle(h http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
+type jsonpResponse struct {
+	Meta map[string]interface{}
+	Data interface{}
+}
+
+func (j *jsonpResponse) MarshalJSON() ([]byte, error) {
+	meta, err := json.Marshal(j.Meta)
+	if err != nil {
+		return nil, err
+	}
+	b := fmt.Sprintf("{\"meta\":%s,\"data\":%s}", meta, j.Data)
+	return []byte(b), nil
+}
+
 type responseBuffer struct {
 	Response http.ResponseWriter // the actual ResponseWriter to flush to
 	Status   int                 // the HTTP response code from WriteHeader
-	PreBody  *bytes.Buffer       // buffer to prepend to the content body
 	Body     *bytes.Buffer       // the response content body
 	Flushed  bool
 }
 
 func NewResponseBuffer(w http.ResponseWriter) *responseBuffer {
 	return &responseBuffer{
-		Response: w, Status: 200,
-		PreBody: &bytes.Buffer{}, Body: &bytes.Buffer{},
+		Response: w, Status: 200, Body: &bytes.Buffer{},
 	}
 }
 
@@ -69,16 +99,7 @@ func (w *responseBuffer) Flush() {
 	if w.Flushed {
 		return
 	}
-
 	w.Response.WriteHeader(w.Status)
-
-	if w.PreBody.Len() > 0 {
-		_, err := w.Response.Write(w.PreBody.Bytes())
-		if err != nil {
-			panic(err)
-		}
-		w.PreBody.Reset()
-	}
 	if w.Body.Len() > 0 {
 		_, err := w.Response.Write(w.Body.Bytes())
 		if err != nil {
@@ -86,6 +107,5 @@ func (w *responseBuffer) Flush() {
 		}
 		w.Body.Reset()
 	}
-
 	w.Flushed = true
 }
